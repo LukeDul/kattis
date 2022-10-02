e61d55d44cbfaeb6f2e0ff3fbcbde48ca6267042
Luke Dulworth <lukedulworth2@gmail.com>
2022-10-01 20:45:11 -0500

    1st attempt

---
+++ chanukah.py
@@ -0,0 +1,21 @@
+#
+# 3
+# 1 8   ->  44
+# 2 1   ->  2
+# 3 10  ->  65
+
+numberOfDataSets = int(input())
+
+dataSets = []
+
+n = 0
+
+while n < numberOfDataSets:
+    curI = input().split(" ")
+    dataSets.append(int(curI[1]))
+    n += 1
+
+print(dataSets)
+
+for i in dataSets:
+    print(i * ((i + 1) / 2) + i)

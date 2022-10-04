4bd153be1224b9327220d4892430da19a5910882
Luke Dulworth <lukedulworth2@gmail.com>
2022-10-03 23:08:27 -0500

    problem description and input

---
+++ upprodun.py
@@ -0,0 +1,29 @@
+"""
+N Teams
+M Rooms
+
+Division of teams needs to be ~even
+
+Number, Number, -> Strings
+Takes two numbers as input, number of teams and number of rooms,
+and outputs a string which indicates the assignments of teams
+
+Input:
+1
+5
+Output:
+*****
+
+Input:
+3
+8
+Output:
+***
+***
+**
+
+
+"""
+
+teams = int(input())
+rooms = int(input())

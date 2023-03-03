909bc9eb926a21944f077bc0e7fe8d9561fe85e3
Luke Dulworth <lukedulworth2@gmail.com>
2023-03-03 11:44:43 -0600

    up to speed

---
+++ java-solutions/SolvingForCarrots.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;p
+class SolvingForCarrots{
+    
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+
+	int contestents = scanner.nextInt();
+
+	int numSolved = scanner.nextInt();
+
+	System.out.(numSolved);
+    }
+}
